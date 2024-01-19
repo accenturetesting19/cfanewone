d3b30624f869333c111c927f7c3157e1e2a25a45
accenturetesting19 <amsapna19@gmail.com>
2024-01-19 16:07:19 +0530

    New code with vulnerability

--- calc.js
+++ calc.js
@@ -6,7 +6,10 @@ const operator = prompt('Enter operator ( either +, -, * or / ): ');
 // take the operand input
 const number1 = parseFloat(prompt('Enter first number: '));
 const number2 = parseFloat(prompt('Enter second number: '));
-
+const number1 = parseFloat(prompt('Enter first number: '));
+const number2 = parseFloat(prompt('Enter second number: '));
+username=admin;
+password=admin;
 let result;
 
 // using if...else if... else
